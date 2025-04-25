40e0d016ad9ebd07953662d90e35ce79fcf03dc4
Pedro DIAS DO VALE <pedro.dias-do-vale@ext.ec.europa.eu>
2025-04-25 17:56:00 +0200

    grid and edit form views with form validation and update support

---
+++ src/main/java/com/pedrodovale/bookshelf/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.pedrodovale.bookshelf.repository;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, UUID> {}
